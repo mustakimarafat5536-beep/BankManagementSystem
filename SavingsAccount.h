63935192e53d64f9bf5336150e4b0ee6a1dff073
Saymon20244129 <mohammadsaymon11@gmail.com>
2026-05-26 02:58:04 +0900

    Update SavingsAccount.h

--- SavingsAccount.h
+++ SavingsAccount.h
@@ -1,16 +1,24 @@
 #ifndef SAVINGSACCOUNT_H
 #define SAVINGSACCOUNT_H
 #include "Account.h"
-class SavingsAccount: public Account {
+class SavingsAccount : public Account {
 public:
-  SavingsAccount(int accNo,
-                string n,
-                double bal):Account(accNo, n, bal){}
-  void display() override {
-    cout<< "\---Savings Account ---\n";
-    Account::display();}
-string getType() override{
-  return "Savings";
-}|
+    SavingsAccount(int accNo,
+                   string n,
+                   double bal)
+    : Account(accNo, n, bal) {
+    }
+    void display() override {
+
+        cout << "\n--- Savings Account ---\n";
+
+        Account::display();
+    }
+    string getType() override {
+
+        return "Savings";
+    }
 };
+
+#endif
 

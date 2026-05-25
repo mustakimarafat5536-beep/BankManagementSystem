02a1ff75644926ff67d1ceb5df00b7ed116fcaee
Saymon20244129 <mohammadsaymon11@gmail.com>
2026-05-26 02:55:56 +0900

    Update CurrentAccount.h

--- CurrentAccount.h
+++ CurrentAccount.h
@@ -1,17 +1,29 @@
 #ifndef CURRENTACCOUNT_H
 #define CURRENTACCOUNT_H
+
 #include "Account.h"
 class CurrentAccount : public Account {
-punlic:
-    CurrentAccount(int accNo;
-                    string n,
-                    double bal)
-       :Account(accNo, n, bal){}
+
+public:
+
+    CurrentAccount(int accNo,
+                   string n,
+                   double bal)
+
+        : Account(accNo, n, bal) {
+    }
+
     void display() override {
-      cout<< "\n----Current Account ---\n;
-Account::display();
-}
-string getType() override{
-  return "Current";
-}};
-s
+
+        cout << "\n--- Current Account ---\n";
+
+        Account::display();
+    }
+
+    string getType() override {
+
+        return "Current";
+    }
+};
+
+#endif
